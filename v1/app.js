61fe11dc472fc6de8a0ee7ca7a8e961f57b8abb6
Reethik Prasad <reethikprasad@gmail.com>
2020-04-27 18:45:06 +0530

    Created form link

--- v1/app.js
+++ v1/app.js
@@ -1,7 +1,16 @@
 var express = require("express");
 var app =express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
+
+var campgrounds=[
+    {name: "SalmonCreek", image: "https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"},
+    {name: "Baywatch", image: "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"},
+    {name: "justpetter", image: "https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"}
+
+];
 
 
 app.get("/",function(req, res){
@@ -9,13 +18,19 @@ app.get("/",function(req, res){
 });
 
 app.get("/campgrounds", function(req,res){
-    var campgrounds=[
-        {name: "SalmonCreek", image: "https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"},
-        {name: "Baywatch", image: "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"},
-        {name: "justpetter", image: "https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"}
-
-    ]
     res.render("campgrounds",{campgrounds:campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    var name= rq.body.name;
+    var image=req.body.image;
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new",function(req,res){
+    res.render("new.ejs");
 });
 
 app.listen(3000, function() { 
